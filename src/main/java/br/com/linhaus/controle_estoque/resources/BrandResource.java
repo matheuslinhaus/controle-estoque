ebc56a00b40c144c71877f1257c4dd48b809d05b
Matheus R Linhaus <matheuslinhauss@hotmail.com>
2024-12-19 11:31:36 -0300

    Criado processos de atualizar e deltar e inserir marcas

--- src/main/java/br/com/linhaus/controle_estoque/resources/BrandResource.java
+++ src/main/java/br/com/linhaus/controle_estoque/resources/BrandResource.java
@@ -1,19 +1,30 @@
 package br.com.linhaus.controle_estoque.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.linhaus.controle_estoque.entities.Brand;
+import br.com.linhaus.controle_estoque.entities.Vape;
 import br.com.linhaus.controle_estoque.entities.enums.ProductType;
 import br.com.linhaus.controle_estoque.services.BrandService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -34,6 +45,29 @@ public class BrandResource {
 	@GetMapping()
 	public List<Brand> getBrandsByProductType(@RequestParam int productType) {
 		return service.findByProductType(ProductType.valueOf(productType));
+	}
+	
+	@Operation(summary = "Criar marca", description = "Método para criar marca no Banco de dados uma nova camisa", tags = "Brand", responses = {
+			@ApiResponse(responseCode = "201", description = "Inserido com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(example = ""))) })
+	@PostMapping
+	public ResponseEntity<Brand> insert(@RequestBody Brand obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@Operation(summary = "Deletar marca", description = "Método para deletar marca no Banco de dados", tags = "Brand")
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@Operation(summary = "Editar marca", description = "Método para editar marca no Banco de dados", tags = "Brand")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Brand> update(@PathVariable Long id, @RequestBody Brand obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
